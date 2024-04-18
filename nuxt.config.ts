4460fe37ebf8bc835754d31abb87332dfb6fddd3
Muridae-dev <92300249+Muridae-dev@users.noreply.github.com>
2024-04-18 15:49:41 +0200

    basic integration of carousel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
+
   vite: {
     css: {
       preprocessorOptions: {
@@ -13,4 +15,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  modules: ["vue3-carousel-nuxt"]
 });
